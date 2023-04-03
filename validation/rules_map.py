952e8a7a4a6a7cf1ef1395c876655a8df506f899
celyes <celyes02@gmail.com>
2023-04-03 16:42:10 +0100

    add more rules

--- validation/rules_map.py
+++ validation/rules_map.py
@@ -1,10 +1,17 @@
+from validation.rules.Gt import Gt
 from validation.rules.DateAfter import DateAfter
 from validation.rules.DateAfterOrEqual import DateAfterOrEqual
 from validation.rules.ArrayOf import ArrayOf
 from validation.rules.DateBefore import DateBefore
 from validation.rules.DateBeforeOrEqual import DateBeforeOrEqual
+from validation.rules.Decimal import Decimal
 from validation.rules.InArray import InArray
 from validation.rules.Array import Array
+from validation.rules.Integer import Integer
+from validation.rules.Json import Json
+from validation.rules.Max import Max
+from validation.rules.Min import Min
+from validation.rules.Nullable import Nullable
 from validation.rules.String import String
 from validation.rules.Required import Required
 from validation.rules.Numeric import Numeric
@@ -13,6 +20,7 @@ from validation.rules.Between import Between
 rules_to_objects_map = {
     "between": Between,
     "string": String,
+    "int": Integer,
     "required": Required,
     "numeric": Numeric,
     "in_array": InArray,
@@ -22,4 +30,10 @@ rules_to_objects_map = {
     "date_after_or_equal": DateAfterOrEqual,
     "date_before": DateBefore,
     "date_before_or_equal": DateBeforeOrEqual,
+    "min": Min,
+    "max": Max,
+    "json": Json,
+    "nullable": Nullable,
+    "decimal": Decimal,
+    "gt": Gt,
 }
