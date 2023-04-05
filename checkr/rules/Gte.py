9e13fe72aae12361e77faf50fd9ab063fc55d8a2
celyes <celyes02@gmail.com>
2023-04-05 15:15:28 +0100

    fix bugs #17

--- checkr/rules/Gte.py
+++ checkr/rules/Gte.py
@@ -6,12 +6,12 @@ from .BaseRule import BaseRule
 class Gte(BaseRule):
     def validate(self, attribute: str, value: Any) -> bool:
         try:
-            return value >= float(self.payload[0])
+            return float(value) >= float(self.payload)
         except TypeError:
             return False
 
     def message(self, attribute: str) -> str:
         try:
-            return f"'{attribute}' is not greater than or equal to {self.payload[0]}"
+            return f"'{attribute}' is not greater than or equal to {self.payload}"
         except TypeError:
             return "You haven't specified any value for comparison"
